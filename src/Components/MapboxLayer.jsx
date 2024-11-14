73bfe5bd41c6fa8333ce69b4631d8485d1b5cf28
RicardoBachmann <ricardo.bachmann@icloud.com>
2024-11-14 09:46:00 +0100

    Add mapbox token and deployed project with vercel

--- src/Components/MapboxLayer.jsx
+++ src/Components/MapboxLayer.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
+
 import "mapbox-gl/dist/mapbox-gl.css";
 import styled from "styled-components";
 
 export default function MapboxLayer() {
+  const mapRef = useRef(); // presist the map instance throuhout the lifecycle of this component
+  const mapContainerRef = useRef(); // exposes the map container and tell Mapbox where to create the map
+
+  useEffect(() => {
+    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-83.0458, 42.3314],
+      zoom: 12,
+    });
+    return () => {
+      mapRef.current.remove();
+    };
+  }, []);
+
   return (
     <>
-      <MapContainer />
+      <MapContainer ref={mapContainerRef} />
     </>
   );
 }
