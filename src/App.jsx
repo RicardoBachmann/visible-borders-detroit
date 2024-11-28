ad9f4e4430478857e018559748400f906cada342
RicardoBachmann <ricardo.bachmann@icloud.com>
2024-11-28 09:05:10 +0100

    Create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import MapboxLayer from "./Components/MapboxLayer";
 import { ThemeProvider } from "styled-components";
-
+import Header from "./Components/Header.jsx";
 import { theme } from "./theme.js";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Header />
       <MapboxLayer />
     </ThemeProvider>
   );
